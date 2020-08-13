ac42dca0bbfbde47ec21c1017c0e64557affe855
tinaxd <31384595+tinaxd@users.noreply.github.com>
2020-08-13 14:16:57 +0900

    登録できるようになった

--- Todo/MainPage.xaml.cs
+++ Todo/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
 
 namespace Todo
 {
@@ -13,6 +15,25 @@ namespace Todo
         public MainPage()
         {
             InitializeComponent();
+            Todos = new ObservableCollection<Todo>();
+
+            Todos.Add(new Todo("tesuto"));
+
+            todoList.BindingContext = this;
         }
+
+        void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("userInput: " + userInput.Text);
+            if (!userInput.Text.Equals(""))
+            {
+                Todos.Add(new Todo(userInput.Text));
+                Debug.WriteLine("Number of Todos: " + Todos.Count);
+            }
+
+            userInput.Text = "";
+        }
+
+        public ObservableCollection<Todo> Todos { get; private set; }
     }
 }
---
+++ Todo/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Todo
+{
+    public class Todo
+    {
+        public string Name { get; set; }
+
+        public Todo(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
